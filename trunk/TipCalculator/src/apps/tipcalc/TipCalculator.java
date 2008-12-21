3f5c6b7c52bdb52fff19174075a189de09b34e72
bryan.dong <bryan.dong@586ac928-a54a-0410-9120-e9e51dd1cab5>
2008-12-21 10:50:00 +0000

    Adds some functionality, formatting for answer.

--- trunk/TipCalculator/src/apps/tipcalc/TipCalculator.java
+++ trunk/TipCalculator/src/apps/tipcalc/TipCalculator.java
@@ -1,7 +1,14 @@
 package apps.tipcalc;
 
+import java.text.NumberFormat;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TipCalculator extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +16,34 @@ public class TipCalculator extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final TextView answer = (TextView)findViewById(R.id.ResultText);
+        final EditText bill = (EditText)findViewById(R.id.Text1);
+        final EditText percent = (EditText)findViewById(R.id.Text2);
+        
+        
+        
+        
+        final Button button = (Button)findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                double billVal = Double.parseDouble(bill.getText().toString());
+                double percentVal = Double.parseDouble(percent.getText().toString());
+                
+                double ans = billVal * percentVal/100.0;
+                double ans2 = billVal + ans;
+                answer.setText("Tip is " + formatDollars(ans) + "\n" + "Total is " + formatDollars(ans2));
+            }
+        });
+        
+        
     }
+    
+    private String formatDollars(double s){
+    	String ans = null;
+   		NumberFormat formatter = NumberFormat.getCurrencyInstance();
+   		ans = formatter.format(s);
+    	
+    	return ans;
+    }    
 }
