d97dfe117d37f287980b40ff75b319c671fd30a8
kadartcomics@gmail.com <kadartcomics@gmail.com@586ac928-a54a-0410-9120-e9e51dd1cab5>
2008-12-22 05:50:56 +0000

    added a dropdown picker for preset percent values.

--- trunk/TipCalculator/src/apps/tipcalc/TipCalculator.java
+++ trunk/TipCalculator/src/apps/tipcalc/TipCalculator.java
@@ -6,12 +6,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class TipCalculator extends Activity {
-    /** Called when the activity is first created. */
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,22 +24,45 @@ public class TipCalculator extends Activity {
         final EditText bill = (EditText)findViewById(R.id.Text1);
         final EditText percent = (EditText)findViewById(R.id.Text2);
         
+        final Spinner s1 = (Spinner) findViewById(R.id.Spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.tipChoices, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(adapter);
         
-        
+        //TODO: Clear percent field when spinner is selected.
+        //TODO: Give spinner default text that carries no value,
+        //and put spinner back to default text when percent field is selected.
         
         final Button button = (Button)findViewById(R.id.Button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 double billVal = Double.parseDouble(bill.getText().toString());
-                double percentVal = Double.parseDouble(percent.getText().toString());
+                double percentVal = 0.0;
+                if(percent.getText().toString() != null && !percent.getText().toString().equals("")){
+                	percentVal = Double.parseDouble(percent.getText().toString());
+                }
+                //XXX: figure out how to set up the xml array to have values corresponding to the strings
+                //otherwise we end up with the proceeding fail.
+                else if (((String)s1.getSelectedItem()).equals(((String)s1.getAdapter().getItem(0)))){
+                	percentVal = 10.0;
+                }
+                else if (((String)s1.getSelectedItem()).equals(((String)s1.getAdapter().getItem(1)))){
+                	percentVal = 12.0;
+                }
+                else if (((String)s1.getSelectedItem()).equals(((String)s1.getAdapter().getItem(2)))){
+                	percentVal = 15.0;
+                }
+                else if (((String)s1.getSelectedItem()).equals(((String)s1.getAdapter().getItem(3)))){
+                	percentVal = 18.0;
+                }
                 
                 double ans = billVal * percentVal/100.0;
                 double ans2 = billVal + ans;
+                System.out.println("Tip Calculated!");
                 answer.setText("Tip is " + formatDollars(ans) + "\n" + "Total is " + formatDollars(ans2));
             }
         });
-        
-        
     }
     
     private String formatDollars(double s){
@@ -45,5 +71,5 @@ public class TipCalculator extends Activity {
    		ans = formatter.format(s);
     	
     	return ans;
-    }    
+    }
 }
